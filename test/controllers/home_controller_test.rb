0e34e054d14c6b6141311ad71b28584a4d023e80
AndresSalcedo <a.salcedo.c@gmail.com>
2016-05-15 18:17:39 -0400

    added tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,4 +4,23 @@ class HomeControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "render 200" do
+    post :buscar, tag: 'santiago', access_token: '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
+    assert_response :success
+  end
+
+  test "render 404" do
+    post :buscar
+    assert_response 400
+  end
+
+  test "posts ok" do
+    assert_not_equal false, @controller.send(:get_posts, 'santiago', '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402')
+  end
+
+  test "total ok" do
+    assert_not_equal false, @controller.send(:get_total, 'santiago', '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402')
+  end
+
 end
